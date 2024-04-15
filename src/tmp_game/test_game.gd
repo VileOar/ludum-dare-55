9441a02c49e6e6a7130f1c93fc9debcf826e1e9a
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2024-04-15 22:33:32 +0100

    small test_game fixes

--- src/tmp_game/test_game.gd
+++ src/tmp_game/test_game.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 
 @onready var _hud: HUDLogic = %Hud
-@onready var _player: Player = %Player
+@onready var _player : Player = $Player
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("oi")
 	_player.new_next_item.connect(_hud._on_new_next_item)
